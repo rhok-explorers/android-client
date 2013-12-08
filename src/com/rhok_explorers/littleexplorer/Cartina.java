8d5cfa1fc216c7262eb71a5b74f4ae3b0a3086d6
BFranz <franzb475@gmail.com>
2013-12-08 15:40:30 +0100

    Boschini KING

--- src/com/rhok_explorers/littleexplorer/Cartina.java
+++ src/com/rhok_explorers/littleexplorer/Cartina.java
@@ -1,30 +1,46 @@
 package com.rhok_explorers.littleexplorer;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.maps.MapActivity;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.Toast;
+import com.google.android.gms.maps.MapFragment;
+public class Cartina extends Activity {
 
-public class Cartina extends MapActivity {
+	private GoogleMap googleMap;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_cartina);
+		try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.cartina, menu);
-		return true;
-	}
-
-	@Override
-	protected boolean isRouteDisplayed() {
-		// TODO Auto-generated method stub
-		return false;
-	}
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            googleMap.setMyLocationEnabled(true);
+            googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
 
 }
